785d8571bff50304f2409242df421de486fd565b
millyard <115674329+millyard@users.noreply.github.com>
2023-04-14 12:00:44 -0500

    worked on generating random qoutes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import quotes from "./dummyAPI.js";
 
-// you can access individual quotes from the quotes array like this:
-console.log(quotes[0].text); // outputs the text of the quote
-console.log(quotes[0].author); // outputs the author of the quote
+const button = document.querySelector("button");
+const quoteDisplay = document.querySelector("div");
 
+function generateQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
 
-// your job is to use DOM manipulation to display a random quote in the index.html page that is retrieved from the quotes array
+  quoteDisplay.textContent = randomQuote.text + " - " + randomQuote.author;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  button.addEventListener("click", generateQuote);
+});
